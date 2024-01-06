7e1e31ba9ee9bd4953f400ef0abb5512459992c9
Jay Abhani <abhani57@gmail.com>
2024-01-06 23:09:02 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="jay.jpg" alt="jay"></img>
+}
+
+function Intro() {
+  return <p>I am a dedicated Master Trainer at Edunet Foundation with a profound passion for teaching and empowering individuals to excel in Full Stack Web Development.</p>
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML & CSS" color="#1abc9c" />
+      <Skill skill="JavaScript" color="#e67e22" />
+      <Skill skill="ReactJS" color="#3498db" />
+      <Skill skill="NodeJS" color="#27ae60" />
+      <Skill skill="Express" color="#e74c3c" />
+      <Skill skill="MongoDB" color="#8e44ad" />
+    </div>
+  )
+}
+
+function Skill(props) {
+  const style = {
+    backgroundColor: props.color
+  }
+  return <p className="skill" style={style}>{props.skill}</p>
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
